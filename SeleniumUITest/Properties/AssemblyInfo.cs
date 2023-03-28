af364611f024c53fa571c8b7ccd7d0c460e6d17c
anna.starling <anna.starling@bbconsult.co.uk>
2023-03-28 20:54:02 +0200

    Add project files.

---
+++ SeleniumUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("644eca40-f173-4c26-b701-47b46e39cec6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumUITest/SampleTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumUITest
+{
+    [TestClass]
+    public class SampleTest
+    {
+        string email = GenerateRandomEmail();
+        WebDriver driver;
+
+        [TestInitialize]
+        public void Init()
+        {
+            //Create an instance for webdriver 
+            driver = new ChromeDriver();
+            //navigate
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            //Maximaze browser window
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            try
+            {
+                
+                string title = driver.Title;
+                Assert.AreEqual(title, "Demo Web Shop");
+
+                driver.FindElement(By.ClassName("ico-register")).Click();
+                driver.FindElement(By.Id("gender-male")).Click();
+                driver.FindElement(By.Id("FirstName")).SendKeys("testest");
+                driver.FindElement(By.Id("LastName")).SendKeys("testest");
+
+                driver.FindElement(By.Id("Email")).SendKeys(email);
+                Console.WriteLine(email);
+
+                driver.FindElement(By.Id("Password")).SendKeys("testest");
+                driver.FindElement(By.Id("ConfirmPassword")).SendKeys("testest");
+
+                driver.FindElement(By.Id("register-button")).Click();
+
+                String message = driver.FindElement(By.ClassName("result")).Text;
+                Assert.AreEqual(message, "Your registeretion complited");
+
+                IWebElement emailAccount = driver.FindElement(By.XPath("//*[text()='" + email + "']"));
+                Assert.IsTrue(emailAccount.Displayed);
+
+                driver.FindElement(By.ClassName("ico-logout")).Click();
+                
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+
+        [TestCleanup]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+
+
+        public static string GenerateRandomEmail()
+        {
+            return string.Format("{0}{1}@{2}.com","sotwaretesteranna+", GenerateRandomNumberString(3), "testdomain");
+        }
+
+        
+
+        public static string GenerateRandomNumberString(int length)
+        {
+            string allowedChars = "abcd1234567890";
+            var rnd = SeedRandom();
+
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = allowedChars[rnd.Next(allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+
+        private static Random SeedRandom()
+        {
+            return new Random(Guid.NewGuid().GetHashCode());
+        }
+    }
+}
