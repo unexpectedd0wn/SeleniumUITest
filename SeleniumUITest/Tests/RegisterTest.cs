80b57fbdb4fe8c384d2b9458e6bac88334ab7b64
anna.starling <anna.starling@bbconsult.co.uk>
2023-03-28 21:05:57 +0200

    #Adding the file structure

---
+++ SeleniumUITest/Tests/RegisterTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SeleniumUITest.Tests
+{
+    [TestClass]
+    public class RegisterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
